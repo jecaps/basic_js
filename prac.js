2d90faa8ba1c2af9a80705a25a4da98beb8ec1ee
Jerika Capila <jerikacapila@gmail.com>
2021-10-17 20:25:05 +0200

    Understanding Uninitialized Variables

--- prac.js
+++ prac.js
@@ -9,3 +9,15 @@ b = a;
 
 // initialize a variable to an initial value in the same line as it is declared
 var a = 9;
+
+/* When JavaScript variables are declared, they have an initial value of undefined.
+number + undefined = NaN (Not a Number)
+string + undefined = "undefined" */
+
+var a;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
