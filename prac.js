5af87f6df878454d01c4d94c06986bdb9bf228d7
Jerika Capila <jerikacapila@gmail.com>
2021-10-18 20:19:24 +0200

    Passing Values to Functions with Arguments

--- prac.js
+++ prac.js
@@ -229,4 +229,11 @@ function reusableFunction() {
   console.log("Hi World");
 }
 
-reusableFunction();
+/* Parameters are variables that act as placeholders for the values that are to be input to a function when it is called. 
+  When a function is defined, it is typically defined along with one or more parameters. 
+  The actual values that are input (or "passed") into a function when it is called are known as arguments. */
+
+function functionWithArgs(p1, p2) {
+  console.log(p1 + p2);
+}
+functionWithArgs(42, 78);
