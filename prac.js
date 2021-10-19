c4758b4961e2b917bafd71816424cdbcf6db979c
Jerika Capila <jerikacapila@gmail.com>
2021-10-19 20:09:29 +0200

    Logical Order in If Else Statements

--- prac.js
+++ prac.js
@@ -467,4 +467,29 @@ function testElseIf(val) {
     return "Between 5 and 10";
   }
 }
-console.log(testElseIf(7));
+testElseIf(7);
+
+// The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+
+console.log(foo(0));
+console.log(bar(0));
