5402294e99dce5f1298cb65628ab6f40a5da8ed4
Jerika Capila <jerikacapila@gmail.com>
2021-10-20 19:32:32 +0200

    Counting Cards

--- prac.js
+++ prac.js
@@ -636,3 +636,40 @@ function abTest(a, b) {
   return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 console.log(abTest(4, 9));
+
+//
+
+var count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1;
+      break;
+  }
+
+  var action = " Hold";
+  if (count > 0) {
+    action = " Bet";
+  }
+
+  return count + action;
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
