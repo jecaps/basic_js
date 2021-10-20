2245b67612e47832b6a1086a26cc7fb33b1277a3
Jerika Capila <jerikacapila@gmail.com>
2021-10-20 15:15:06 +0200

    Replacing If Else Chains with Switch

--- prac.js
+++ prac.js
@@ -575,4 +575,34 @@ function sequentialSizes(val) {
   }
   return answer;
 }
-console.log(sequentialSizes(1));
+sequentialSizes(1);
+
+// If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements.
+
+function chainToSwitch(val) {
+  // if (val === "bob") {
+  //   answer = "Marley";
+  // } else if (val === 42) {
+  //   answer = "The Answer";
+  // } else if (val === 1) {
+  //   answer = "There is no #1";
+  // } else if (val === 99) {
+  //   answer = "Missed me by this much!";
+  // } else if (val === 7) {
+  //   answer = "Ate Nine";
+  // }
+
+  switch (val) {
+    case "bob":
+      return "Marley";
+    case 42:
+      return "The Answer";
+    case 1:
+      return "There is no #1";
+    case 99:
+      return "Missed me by this much!";
+    case 7:
+      return "Ate Nine";
+  }
+}
+console.log(chainToSwitch(99));
