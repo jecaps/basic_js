58ab9626678875e55783fa7b73e89edb2bdfd702
Jerika Capila <jerikacapila@gmail.com>
2021-10-20 13:34:45 +0200

    Multiple Identical Options in Switch Statements

--- prac.js
+++ prac.js
@@ -550,4 +550,29 @@ function switchOfStuff(val) {
   }
   return answer;
 }
-console.log(switchOfStuff("a"));
+switchOfStuff("a");
+
+// If you have multiple inputs with the same output, you can represent them in a switch statement like this:
+
+function sequentialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+console.log(sequentialSizes(1));
