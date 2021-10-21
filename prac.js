38c730cc78e6510a68ea9cbc661cd6c3f4138c34
Jerika Capila <jerikacapila@gmail.com>
2021-10-21 16:43:42 +0200

    Accessing Nested Objects

--- prac.js
+++ prac.js
@@ -793,3 +793,20 @@ var myMusic = [
     platinum: true,
   },
 ];
+
+//
+
+var ourStorage = {
+  desk: {
+    drawer: "stapler",
+  },
+  cabinet: {
+    "top drawer": {
+      folder1: "a file",
+      folder2: "secrets",
+    },
+    "bottom drawer": "soda",
+  },
+};
+console.log(ourStorage.cabinet["top drawer"].folder2);
+console.log(ourStorage.desk.drawer);
