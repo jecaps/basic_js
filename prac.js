98570d06faed198a32a2895d7e7e3ca526c491e6
Jerika Capila <jerikacapila@gmail.com>
2021-10-21 14:43:15 +0200

    Using Objects for Lookups

--- prac.js
+++ prac.js
@@ -745,3 +745,22 @@ console.log(myDog);
 delete myDog.tails;
 
 console.log(myDog);
+
+/* Objects can be thought of as a key/value storage, like a dictionary. 
+If you have tabular data, you can use an object to lookup values rather than a switch statement or an if/else chain. This is most useful when you know that your input data is limited to a certain range. */
+
+function phoneticLookup(val) {
+  var result = "";
+  var lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
+  return result;
+}
+
+console.log(phoneticLookup("delta"));
