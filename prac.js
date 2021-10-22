9c83e19e4d12a1f439ea65e860a662abaf2fa8f1
Jerika Capila <jerikacapila@gmail.com>
2021-10-22 19:40:43 +0200

    Profile Lookup

--- prac.js
+++ prac.js
@@ -957,3 +957,53 @@ function summ(arr, n) {
 }
 
 console.log(summ([2, 3, 4, 5], 3));
+
+/* A lookUpProfile function that takes name and a property (prop) as arguments has been pre-written for you.
+  The function should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+  If both are true, then return the "value" of that property.
+  If name does not correspond to any contacts then return the string No such contact.
+  If prop does not correspond to any valid properties of a contact found to match name then return the string No such property.
+ */
+
+var contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  var contact;
+  for (contact of contacts) {
+    if (name == contact["firstName"]) {
+      if (contact.hasOwnProperty(prop)) {
+        return contact[prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+console.log(lookUpProfile("Akira", "lastName"));
