ef667b0c9818fef0cfc4c47fcccdb4aa6bd97a9f
Jerika Capila <jerikacapila@gmail.com>
2021-10-23 15:05:20 +0200

    Use the parseInt Function

--- prac.js
+++ prac.js
@@ -1031,3 +1031,12 @@ function randomRange(myMin, myMax) {
 }
 
 console.log(randomRange(5, 15));
+
+/* parseInt() function converts the string to the integer .
+  If the first character in the string can't be converted into a number, then it returns NaN. */
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+console.log(convertToInteger("56"));
