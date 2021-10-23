617ecaf5afa4e4b45429b195a12a5b14f0cda022
Jerika Capila <jerikacapila@gmail.com>
2021-10-23 14:59:12 +0200

    Generate Random Whole Numbers within a Range

--- prac.js
+++ prac.js
@@ -1023,3 +1023,11 @@ function randomWholeNum() {
 }
 
 console.log(randomWholeNum());
+
+// To generate a random whole number that falls within a range of two specific numbers.
+
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
+
+console.log(randomRange(5, 15));
