b9980898b10f7b9b3bd9f71bc5a4109456d62446
Jerika Kanz <jerikacapila@gmail.com>
2021-10-23 15:45:39 +0200

    Use the Conditional (Ternary) Operator

--- prac.js
+++ prac.js
@@ -1050,3 +1050,24 @@ function convertToInteger(str) {
 }
 
 console.log(convertToInteger("10011"));
+
+/* The conditional operator, also called the ternary operator, can be used as a one line if-else expression.
+  The syntax is a ? b : c, where a is the condition, b is the code to run when the condition returns true, and c is the code to run when the condition returns false.
+ */
+
+function findGreater(a, b) {
+  if (a > b) {
+    return "a is greater";
+  } else {
+    return "b is greater or equal";
+  }
+} //this function...
+
+function findGreater(a, b) {
+  return a > b ? "a is greater" : "b is greater or equal";
+} //...can also be written like this
+
+function checkEqual(a, b) {
+  return a == b ? "Equal" : "Not Equal";
+}
+console.log(checkEqual(1, 2));
