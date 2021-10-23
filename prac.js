5f5dd4df8e4152802bc0e1494dc828fe1f152a9a
Jerika Kanz <jerikacapila@gmail.com>
2021-10-23 15:39:43 +0200

    Use the parseInt Function with a Radix

--- prac.js
+++ prac.js
@@ -1040,3 +1040,13 @@ function convertToInteger(str) {
 }
 
 console.log(convertToInteger("56"));
+
+/* The parseInt() function parses a string and returns an integer. 
+  It takes a second argument for the radix, which specifies the base of the number in the string.  
+  The radix can be an integer between 2 and 36. */
+
+function convertToInteger(str) {
+  return parseInt(str, 36);
+}
+
+console.log(convertToInteger("10011"));
